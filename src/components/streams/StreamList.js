e419afb2ffd27714968084e9795eeb51ea7e4666
DS <dianakondro@gmail.com>
2021-03-18 15:39:12 +0100

    bug fix

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
-import streams from '../../apis/streams';
 
 class StreamList extends React.Component {
   componentDidMount() {
@@ -10,42 +9,37 @@ class StreamList extends React.Component {
   }
 
   renderAdmin(stream) {
-    if(stream.userId === this.props.currentUserId) {
-      console.log(streams)
+    if (stream.userId === this.props.currentUserId) {
       return (
-      <div className="right floated content">
-        <button className="ui button primary">
-          Edit
-        </button>
-        <button className="ui button negative">
-          Delete
-        </button>
-      </div>
-      )
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
     }
   }
 
   renderList() {
     return this.props.streams.map(stream => {
-      return(
+      return (
         <div className="item" key={stream.id}>
           {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
-          {stream.title}
-            <div className="decription">{stream.decription}</div>
+            {stream.title}
+            <div className="description">{stream.description}</div>
           </div>
         </div>
       );
-    })
+    });
   }
 
-  renderCreate(){
+  renderCreate() {
     if (this.props.isSignedIn) {
       return (
         <div style={{ textAlign: 'right' }}>
           <Link to="/streams/new" className="ui button primary">
-          Create Stream
+            Create Stream
           </Link>
         </div>
       );
@@ -55,20 +49,23 @@ class StreamList extends React.Component {
   render() {
     return (
       <div>
-        <h2>streams</h2>
+        <h2>Streams</h2>
         <div className="ui celled list">{this.renderList()}</div>
-        {this.renderCreate}
+        {this.renderCreate()}
       </div>
-    )
-  };
+    );
+  }
 }
 
 const mapStateToProps = state => {
-  return { 
-    //streams: Object.values(state.streams),
+  return {
+    streams: Object.values(state.streams),
     currentUserId: state.auth.userId,
     isSignedIn: state.auth.isSignedIn
   };
 };
 
-export default connect(mapStateToProps, { fetchStreams })(StreamList);
+export default connect(
+  mapStateToProps,
+  { fetchStreams }
+)(StreamList);
